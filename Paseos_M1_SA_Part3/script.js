2a5b3ca0861da45e31c944ee62d9806e8452aa26
seanpaseos <sjPaseos@mcm.edu.ph>
2025-02-05 00:49:25 +0800

    Bago

---
+++ Paseos_M1_SA_Part3/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const projectLinks = document.querySelectorAll('.project-link');
+    projectLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            
+        });
+    });
+});
